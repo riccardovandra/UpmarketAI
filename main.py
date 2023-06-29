dbe43c78d9c76a28562deb79fe1cb88afe729414
Riccardo Vandra <riccardovandra@Riccardos-Laptop.local>
2023-06-29 09:05:42 +0200

    improved module import + if name main

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import streamlit as st
-from utils import *
+from utils import workflow
 
-st.title('Business Profile Creation')
-website_url = st.text_input('Insert Website URL Here')
+def main():
+    st.title('Business Profile Creation')
+    website_url = st.text_input('Insert Website URL Here')
 
-if website_url:
-    description = workflow.get_business_description(website_url)
-    st.header("Business Description")
-    st.write(description)
+    if website_url:
+        description = workflow.get_business_description(website_url)
+        st.header("Business Description")
+        st.write(description)
 
-    description_short = workflow.get_business_description_short(description)
+        description_short = workflow.get_business_description_short(description)
 
-    st.header("Description (Short)")
-    st.write(description_short)
-    
-    audience_persona = workflow.get_audience_persona(description)
-    st.header("Audience Persona")
-    st.write(audience_persona)
+        st.header("Description (Short)")
+        st.write(description_short)
+        
+        audience_persona = workflow.get_audience_persona(description)
+        st.header("Audience Persona")
+        st.write(audience_persona)
+
+if __name__ == '__main__':
+    main()
